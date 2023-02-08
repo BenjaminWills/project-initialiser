8fc8f7b249cfaeabcd494b46562ccf3c08885708
Benjaminwills <benjaminwills047@gmail.com>
2023-02-08 19:23:38 +0000

    add a make logger function

---
+++ src/make_logger.py
@@ -0,0 +1,35 @@
+import logging
+import os
+
+
+def add_file_output(logger: logging.Logger, logging_path: str, formatter_style: str):
+    file_handler = logging.FileHandler(logging_path)
+    formatter = logging.Formatter(formatter_style)
+
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+
+
+def make_logger(
+    logging_path: str = os.getcwd(),
+    save_logs: bool = False,
+    formatter_style: str = "%(asctime)s - %(name)s - %(levelname)s :: %(message)s",
+    logger_name: str = __name__,
+    logger_level: int = logging.INFO,
+) -> logging.Logger:
+
+    # Create logger
+    logger = logging.getLogger(name=logger_name)
+    # Set urgency level
+    logger.setLevel(logger_level)
+
+    if save_logs:
+        add_file_output(logger, logging_path, formatter_style)
+
+    return logger
+
+
+if __name__ == "__main__":
+    logger = make_logger()
+    print(logger.name)
